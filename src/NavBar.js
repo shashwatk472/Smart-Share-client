4253de67f66f3314cd7820ccf4870675d42c1940
SKD <sarthakd762@gmail.com>
2022-06-01 18:59:23 +0530

    upload and dashboard 1.0

---
+++ src/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./NavBar.css";
+function NavBar() {
+  return (
+    <div className="navBar">
+      <h3>Smart Share</h3>
+    </div>
+  );
+}
+
+export default NavBar;
